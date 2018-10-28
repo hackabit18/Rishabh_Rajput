a2333e536da3f3c44fb8693249bbd2353dac0251
catmss <ss.mrinalini@gmail.com>
2018-10-28 06:58:23 +0530

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("test_data.csv", header=None, index_col=False) 
-# Preview the first 5 lines of the loaded data 
-data = data.sample(frac=1).reset_index(drop=True)
-print(data)
